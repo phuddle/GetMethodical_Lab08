b9cb2ddcc052591f15cb07735dc37a41047a19dc
phuddle <158978721+phuddle@users.noreply.github.com>
2024-03-11 11:39:19 -0400

    Update FavNumbers.java

--- src/FavNumbers.java
+++ src/FavNumbers.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class FavNumbers {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int favoriteInt = SafeInput.getInt(in, "Enter your favorite integer");
-        double favoriteDouble = SafeInput.getDouble(in, "Enter your favorite double");
+        Scanner in = new Scanner(System.in); //scanner
+        int favoriteInt = SafeInput.getInt(in, "Enter your favorite integer"); //getting int
+        double favoriteDouble = SafeInput.getDouble(in, "Enter your favorite double"); //getting dbl
         System.out.println("Your favorite integer: " + favoriteInt);
         System.out.println("Your favorite double: " + favoriteDouble);
     }
