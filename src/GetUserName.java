5115c2c3c2217946820c8e5d8d86737848c6666c
phuddle <158978721+phuddle@users.noreply.github.com>
2024-03-11 11:38:31 -0400

    Update GetUserName.java

--- src/GetUserName.java
+++ src/GetUserName.java
@@ -4,11 +4,11 @@
     {
         public static void main(String[] args)
         {
-            Scanner in = new Scanner(System.in);
+            Scanner in = new Scanner(System.in); //create scanner
             String firstName = "";
             String lastName = "";
-            firstName = SafeInput.getNonZeroLenString(in, "Enter your first name");
-            lastName = SafeInput.getNonZeroLenString(in, "Enter your last name");
+            firstName = SafeInput.getNonZeroLenString(in, "Enter your first name"); //first name, making sure length =/= 0
+            lastName = SafeInput.getNonZeroLenString(in, "Enter your last name");//last name, making sure length =/= 0 
                 System.out.println("\nYour full name is: " + firstName + " " + lastName);
         }
     }
