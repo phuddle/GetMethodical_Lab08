d911dd6b213de0268e7f8b28e0c784da98bba417
phuddle <158978721+phuddle@users.noreply.github.com>
2024-03-11 11:37:21 -0400

    Update PrettyHeader.java

--- src/PrettyHeader.java
+++ src/PrettyHeader.java
@@ -1,5 +1,5 @@
 public class PrettyHeader {
     public static void main(String[] args) {
-        SafeInput.prettyHeader("Message Centered Here");
+        SafeInput.prettyHeader("Message Centered Here"); //call prettyHeader mthd from SafeInput
     }
 }
