43e0dbcf529bdae7ab6edd75fa23d8fe9d1b75d9
phuddle <158978721+phuddle@users.noreply.github.com>
2024-03-11 11:41:03 -0400

    Update CheckOut.java

--- src/CheckOut.java
+++ src/CheckOut.java
@@ -5,11 +5,11 @@ public class CheckOut {
         Scanner in = new Scanner(System.in);
         double totalCost = 0;
         boolean moreItems;
-        do {
+        do { //loop for user to input item prices
             double price = SafeInput.getRangedDouble(in, "Enter the price of the item ($0.50 - $10.00)", 0.50, 10.00);
-            totalCost += price;
-            moreItems = SafeInput.getYNConfirm(in, "Do you have more items to add?");
-        } while (moreItems);
+            totalCost += price; //adding price to total cost
+            moreItems = SafeInput.getYNConfirm(in, "Do you have more items to add?"); 
+        } while (moreItems); //loop until user has no more inputs
         System.out.printf("Total cost of the items: $%.2f%n", totalCost);
     }
 }
